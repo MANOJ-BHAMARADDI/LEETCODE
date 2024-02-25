e6918c770ef2b333e6b5e6345ed1d7456fd553d6
Manoj Bhamaraddi <141751469+MANOJ-BHAMARADDI@users.noreply.github.com>
2024-02-25 16:13:46 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (35.60%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+         if(arr.length<3){
+          return false;
+         }  
+      int c=0;
+      for(int i=0; i<arr.length; i++){
+          if(arr[i]%2!=0){
+              c++;
+              if(c==3)return true;
+          }
+          else{
+              c=0;
+          }
+      }
+      return false;
+    }
+}
