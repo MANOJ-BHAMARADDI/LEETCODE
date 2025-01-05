0d5c32374e9c3051ca414b1338d90582c3fe328f
Manoj Bhamaraddi <141751469+MANOJ-BHAMARADDI@users.noreply.github.com>
2025-01-05 19:11:59 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.93%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,23 +1,35 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+
+
         if (matrix == null || matrix.length == 0) return false;
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int low = 0, high = rows * cols - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int midValue = matrix[mid / cols][mid % cols]; // Map 1D index to 2D
-
-            if (midValue == target) {
-                return true;
-            } else if (midValue < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j]==target){
+                    return true;
+                }
             }
         }
+
         return false;
+
+        // int rows = matrix.length;
+        // int cols = matrix[0].length;
+        // int low = 0, high = rows * cols - 1;
+
+        // while (low <= high) {
+        //     int mid = (low + high) / 2;
+        //     int midValue = matrix[mid / cols][mid % cols]; // Map 1D index to 2D
+
+        //     if (midValue == target) {
+        //         return true;
+        //     } else if (midValue < target) {
+        //         low = mid + 1;
+        //     } else {
+        //         high = mid - 1;
+        //     }
+        // }
+        // return false;
     }
 }
