4231ae7c186d6a69629d545585b764a0d0125a2d
Manoj Bhamaraddi <141751469+MANOJ-BHAMARADDI@users.noreply.github.com>
2025-01-30 23:28:12 +0530

    Time: 1 ms (23.29%), Space: 50.4 MB (6.13%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,17 +1,29 @@
 class Solution {
     public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length - 1;
+        // int i = 0;
+        // int j = s.length - 1;
 
-        while (i < j) {
-            // Swap s[i] and s[j]
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+        // while (i < j) {
+        //     // Swap s[i] and s[j]
+        //     char temp = s[i];
+        //     s[i] = s[j];
+        //     s[j] = temp;
 
-            // Move towards the center of the array
-            i++;
-            j--;
-        }
+        //     // Move towards the center of the array
+        //     i++;
+        //     j--;
+        // }
+        reverseHelper(s, 0, s.length - 1);
+    }
+    private void reverseHelper(char[] s, int i, int j) {
+        if (i >= j) return; // Base case: when indices cross or are equal stop recursion
+
+        // Swap s[i] and s[j]
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        // Recursive call moving towards the center
+        reverseHelper(s, i + 1, j - 1);
     }
 }
